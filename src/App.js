e7e54387ed493ecb5cb7fe784b7287803e82b718
kimjunho <junho07140714@dgsw.hs.kr>
2023-04-13 09:20:49 +0900

    :sparkle: :: learn props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import Potato from "./potato";
 
+function Food(props) {
+  return <h1>I like {props.name}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
       <Potato />
+      <Food name="kimchi" />
+      <Food name="ramen" />
+      <Food name="samgiopsal" />
+      <Food name="chukumi" />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./index.css";
 import App from "./App";
-import Potato from "./potato";
 
 const root = ReactDOM.createRoot(document.getElementById("potato"));
-root.render(<App /><Potato />);
+root.render(<App />);
