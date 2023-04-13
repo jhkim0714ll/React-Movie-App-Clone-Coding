606f704bdff95f50f8cf4cb3b5782f4cff6fef73
kimjunho <junho07140714@dgsw.hs.kr>
2023-04-13 10:00:19 +0900

    :sparkle: :: learn propTypes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import "./App.css";
 import Potato from "./potato";
 
-function Food(props) {
-  return <h1>I like {props.name}</h1>;
+function Food({ name, image, rating }) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <h4>{rating}</h4>
+      <h4>{image}</h4>
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "kimchi",
+    image: "대충 김치 사진",
+    rating: 5,
+  },
+  {
+    name: "물",
+    image: "대충 물 사진",
+    rating: 3,
+  },
+];
+
+function renderFood(dish) {
+  return <Food name={dish.name} image={dish.image} />;
+}
+
+Food.protoTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 function App() {
   return (
     <div className="App">
       <Potato />
       <Food name="kimchi" />
-      <Food name="ramen" />
-      <Food name="samgiopsal" />
-      <Food name="chukumi" />
+      {foodILike.map((dish) => {
+        <Food name={dish.name} />;
+      })}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
