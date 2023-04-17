b83fcc250b59fd6e571963417f44d3a354ff08e5
kimjunho <junho07140714@dgsw.hs.kr>
2023-04-17 11:10:28 +0900

    :sparkle: :: learn class components and state

---
+++ state/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+}
+
+export default App;
